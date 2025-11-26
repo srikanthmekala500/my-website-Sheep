02e23c069aeb72e48ac5a60eecc0f044d0522e30
srikanth mekala <125475567+srikanthmekala500@users.noreply.github.com>
2025-11-26 09:36:57 +0530

    Update blog.js

--- blog.js
+++ blog.js
@@ -55,17 +55,7 @@ export function initializeBlog(app, storageInstance, blogModalInstance, viewModa
     }
 
     // Initialize the Quill editor
-quillEditor = new Quill('#blogPostEditor', {
-        theme: 'snow',
-        modules: {
-            toolbar: [
-                [{ 'header': [1, 2, 3, false] }],
-                ['bold', 'italic', 'underline', 'link'],
-                [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                ['image', 'code-block', 'clean']
-            ]
-        }
-    });
+quillEditor = new Quill('#blogPostEditor', { theme: 'snow', modules: { toolbar: [ [{ 'header': [1, 2, 3, false] }], ['bold', 'italic', 'underline', 'link'], [{ 'list': 'ordered' }, { 'list': 'bullet' }], ['image', 'code-block', 'clean'] ] } });
 
 
     quillEditor.getModule('toolbar').addHandler('image', imageHandler);
@@ -528,4 +518,5 @@ function handlePaginationClick(e) {
     window.renderCurrentBlogView(); // We'll expose a function from app.js to do this
 
 }
+
 
